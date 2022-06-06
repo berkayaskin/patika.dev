8fa17545c4c8c36e662ad39928a48f82d2f95d0b
berkayaskin <berkayaskiin@gmail.com>
2022-06-07 01:33:46 +0300

    remove iife call

--- patika-data-fetching/app.js
+++ patika-data-fetching/app.js
@@ -1,6 +1,5 @@
 import getData from "./fetch.js";
 
-(async () => {
-	const userData = await getData(1);
-	console.log(userData);
-})();
+const userData = await getData(1);
+
+console.log(userData);
